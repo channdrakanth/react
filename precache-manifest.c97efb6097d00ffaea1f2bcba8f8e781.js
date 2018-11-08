22c4be837d056f43af6d90c0030bf42c3fdf7e11
chandrakanth.polepal <1729.pck@gmail.com>
2018-11-08 14:56:26 +0530

    Updates

---
+++ precache-manifest.c97efb6097d00ffaea1f2bcba8f8e781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c557fa3baaae86c4890",
+    "url": "/channdrakanth/myRectApp/static/js/runtime~main.8c557fa3.js"
+  },
+  {
+    "revision": "a46a5c60e004d1d13aa0",
+    "url": "/channdrakanth/myRectApp/static/js/main.a46a5c60.chunk.js"
+  },
+  {
+    "revision": "d5bf6c368cfcef7c9306",
+    "url": "/channdrakanth/myRectApp/static/js/1.d5bf6c36.chunk.js"
+  },
+  {
+    "revision": "a46a5c60e004d1d13aa0",
+    "url": "/channdrakanth/myRectApp/static/css/main.ac201010.chunk.css"
+  },
+  {
+    "revision": "d5bf6c368cfcef7c9306",
+    "url": "/channdrakanth/myRectApp/static/css/1.52a1203b.chunk.css"
+  },
+  {
+    "revision": "02ce2553041b371ed747c23f8e8f4e1d",
+    "url": "/channdrakanth/myRectApp/index.html"
+  }
+];
